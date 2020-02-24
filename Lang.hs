c537ecf7a05117d2e40550ab9d60bfe1eab06007
Cole Swanson <40472043+Coleswn8@users.noreply.github.com>
2020-02-23 16:13:42 -0800

    Added basic core functionality to the language (untested)

--- Lang.hs
+++ Lang.hs
@@ -16,3 +16,43 @@ data Cmd = Push Value | E Expr | S Stmt
 type Queue = [Value]
 type Prog = [Cmd]
 type Domain = Queue -> Maybe Queue
+
+cmd :: Cmd -> Domain
+cmd (Push v) q =Just (q ++ [v])
+cmd (E e) q = expr e q
+cmd (S s) q = stmt s q
+
+expr :: Expr -> Domain
+expr Add q = case q of 
+                ((I i) : (I j) : qs) -> Just ( qs ++ [I (i+j)] )
+                _                    -> Nothing
+expr Mul q = case q of
+              ((I i) : (I j) : qs) -> Just (qs ++ [I (i*j)])
+              _                    -> Nothing  
+expr Equ q = case q of 
+               ((I i) : (I j) : qs) -> Just (qs ++ [B (i == j)])
+               ((B a) : (B b) : qs)  -> Just (qs ++ [B (a == b)])
+               _                    -> Nothing 
+expr (If t f) q = case q of
+                  (B True : qs)  -> prog t qs
+                  (B False : qs) -> prog f qs
+                  _              -> Nothing 
+
+stmt :: Stmt -> Domain
+stmt (While e c) q = case (expr e q) of 
+                     (Just ((B True):qs)) -> case (cmd c qs) of
+                                             (Just q) -> stmt (While e c) q
+                                             _        -> Nothing
+                     (Just (_:qs))         -> Just (qs)
+                     _                     -> Nothing
+stmt (Begin (c:cs)) q = case (c:cs) of
+                     [] -> Just q
+                     _  -> case (cmd c q) of
+                           (Just q) -> stmt (Begin cs) q
+                           _        -> Nothing
+ 
+
+prog :: Prog -> Domain
+prog (c:cs) q  = case (cmd c q) of
+                 (Just q) -> prog cs q 
+                 _        -> Nothing     
