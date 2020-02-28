1019fbefcc375dbdf4b2d630abfec0b73b60542e
C. I. Swanson <swanscol@oregonstate.edu>
2020-02-27 19:28:16 -0800

    Add syntactic sugar

--- Lang.hs
+++ Lang.hs
@@ -17,6 +17,7 @@ data Expr = Add
           | Div
           | Equ
           | If Prog Prog
+          | Less
    deriving (Eq, Show)
 
 data Stmt = While Expr Cmd
@@ -62,6 +63,12 @@ tupleEqu (T a b) (T c d) = case (a, b, c, d) of
                               _                    -> False
 tupleEqu _       _       = False
 
+tupleLess :: Value -> Value -> Bool
+tupleLess (T a b) (T c d) = case (a, b, c, d) of
+                              (I a, I b, I c, I d) -> a < c && b < d
+                              _                    -> False
+tupleLess _       _       = False
+
 
 expr :: Expr -> Domain
 expr Add q = case q of 
@@ -69,7 +76,7 @@ expr Add q = case q of
                 ( F f : qs )         -> case (prog [f] qs) of 
                                         Just q  -> expr Add q
                                         Nothing -> Nothing  
-                (a : F f : qs )    -> case (prog [f] qs) of 
+                (a : F f : qs )      -> case (prog [f] qs) of 
                                         Just q  -> expr Add (a : q)
                                         Nothing -> Nothing  
                 (T v w : T y z : qs) -> case (v, w, y, z) of
@@ -81,7 +88,7 @@ expr Mul q = case q of
                 ( F f : qs )         -> case (prog [f] qs) of 
                                         Just q  -> expr Mul q
                                         Nothing -> Nothing 
-                (a : F f : qs )    -> case (prog [f] qs) of 
+                (a : F f : qs )      -> case (prog [f] qs) of 
                                         Just q  -> expr Mul (a : q)
                                         Nothing -> Nothing   
                 (T v w : T y z : qs) -> case (v, w, y, z) of
@@ -95,22 +102,34 @@ expr Div q = case q of
                ( F f : qs )         -> case (prog [f] qs) of 
                                        Just q  -> expr Div q
                                        Nothing -> Nothing
-               (a : F f : qs )    -> case (prog [f] qs) of 
+               (a : F f : qs )      -> case (prog [f] qs) of 
                                         Just q  -> expr Div (a : q)
                                         Nothing -> Nothing  
                (T v w : T y z : qs) -> case tupleDiv (T v w) (T y z) of
                                           (Just (T a b)) -> Just (T a b : qs)
                                           _              -> Nothing
+               _                    -> Nothing
 expr Equ q = case q of 
                (I i   : I j   : qs) -> Just (B (i == j) : qs)
                (B a   : B b   : qs) -> Just (B (a == b) : qs)
                ( F f : qs )         -> case (prog [f] qs) of 
                                        Just q  -> expr Equ q
                                        Nothing -> Nothing
-               (a : F f : qs )    -> case (prog [f] qs) of 
+               (a : F f : qs )      -> case (prog [f] qs) of 
                                         Just q  -> expr Equ (a : q)
                                         Nothing -> Nothing  
                (T v w : T y z : qs) -> Just (B (tupleEqu (T v w) (T y z)) : qs)
+               _                    -> Nothing
+expr Less q = case q of 
+               (I i   : I j   : qs) -> Just (B (i < j) : qs)
+               ( F f : qs )         -> case (prog [f] qs) of 
+                                       Just q  -> expr Less q
+                                       Nothing -> Nothing
+               (a : F f : qs )      -> case (prog [f] qs) of 
+                                        Just q  -> expr Less (a : q)
+                                        Nothing -> Nothing  
+               (T v w : T y z : qs) -> Just (B (tupleLess (T v w) (T y z)) : qs) 
+               _                    -> Nothing                
 expr (If t f) q = case q of
                   (B True : qs)  -> prog t qs
                   (B False : qs) -> prog f qs
@@ -138,3 +157,30 @@ prog [] q      = Just q
 prog (c:cs) q  = case cmd c q of
                      (Just q) -> prog cs q 
                      _        -> Nothing
+
+
+-- Syntactic Sugar --
+
+true :: Cmd
+true = Push (B True)
+
+false :: Cmd
+false = Push (B True)
+
+greaterequ :: Cmd
+greaterequ = S (Begin [E Less, notl])
+
+inc :: Cmd
+inc = S (Begin [Push (I 1), E Add])
+
+dec :: Cmd
+dec = S (Begin [Push (I (-1)), E Add])
+
+notl :: Cmd
+notl = E (If [Push (B False)] [Push (B True)])
+
+andl :: Cmd
+andl = E (If [E (If [true] [false])] [E (If [false] [false])]) 
+
+orl :: Cmd
+orl = E (If [E (If [true] [true])] [E (If [true] [false])]) 
